5c051c7256f89734444de2dcb20bf6f2a571ecf1
Raphael Randschau <nicolai86@me.com>
2013-11-15 23:05:50 +0100

    also parse Deployment#deployed_at

--- lib/revisioneer_rails/deployment.rb
+++ lib/revisioneer_rails/deployment.rb
@@ -2,7 +2,7 @@ require "json"
 require "typhoeus"
 
 module RevisioneerRails
-  class Deployment < Struct.new(:sha, :messages)
+  class Deployment < Struct.new(:sha, :deployed_at, :messages)
     def self.load page = 1, limit = 20
       url = ::RevisioneerRails.config.url + "/deployments"
       request = Typhoeus::Request.new(
@@ -14,7 +14,7 @@ module RevisioneerRails
       json = ::JSON.parse response.body
 
       json.map { |deploy_data|
-        self.new(deploy_data[:sha], deploy_data[:messages])
+        self.new(deploy_data["sha"], Time.parse(deploy_data["deployed_at"]), deploy_data["messages"])
       }
     end
   end
