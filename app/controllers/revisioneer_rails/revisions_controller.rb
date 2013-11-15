2a8e5170d0641783e5a70c749368adec2be386d1
Raphael Randschau <nicolai86@me.com>
2013-11-15 22:59:26 +0100

    display deployments

--- app/controllers/revisioneer_rails/revisions_controller.rb
+++ app/controllers/revisioneer_rails/revisions_controller.rb
@@ -3,5 +3,11 @@ module RevisioneerRails
     def index
 
     end
+
+    protected
+    def deployments
+      @deployments ||= ::RevisioneerRails::Deployment.load
+    end
+    helper_method :deployments
   end
 end
